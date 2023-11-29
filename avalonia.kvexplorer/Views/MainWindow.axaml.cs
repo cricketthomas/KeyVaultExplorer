7c064aeeb0b5c960a2969ee817dd2228584014a1
Arthur Thomas <acthomasiv@gmail.com>
2023-11-28 19:53:49 -0500

    wip

---
+++ avalonia.kvexplorer/Views/MainWindow.axaml.cs
@@ -0,0 +1,49 @@
+﻿using avalonia.kvexplorer.ViewModels;
+using avalonia.kvexplorer.Views.Pages;
+using Avalonia.Animation;
+using Avalonia.Controls;
+using Avalonia.Controls.Notifications;
+using Avalonia.Interactivity;
+using Avalonia.Media;
+using Avalonia.Platform;
+using Avalonia.Styling;
+using FluentAvalonia.Styling;
+using FluentAvalonia.UI.Windowing;
+using System;
+using System.Diagnostics;
+
+namespace avalonia.kvexplorer.Views;
+
+public partial class MainWindow : AppWindow
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+        TitleBar.ExtendsContentIntoTitleBar = true;
+        TitleBar.TitleBarHitTestType = TitleBarHitTestType.Complex;
+        //TitleBar.ButtonHoverBackgroundColor = Color.FromArgb(35, 155, 155, 155);
+        App.Current.Resources.TryGetResource("DynamicActiveBackgroundFAColor", null, out var bg);
+        App.Current.Resources.TryGetResource("DynamicActiveBackgroundFAColorMac", null, out var bgMac);
+
+        if (OperatingSystem.IsWindows())
+        {
+            Activated += (sender, e) =>
+            {
+                Background = null;
+            };
+            Deactivated += (sender, e) =>
+            {
+                Background = (IBrush)bg;
+            };
+        }
+        else
+        {
+        
+         Background = (IBrush)bgMac;
+           
+        }
+
+        //TitleBar.ExtendsContentIntoTitleBar = OperatingSystem.IsMacOS() ? true : false;
+        // ExtendClientAreaChromeHints = OperatingSystem.IsMacOS() ? Avalonia.Platform.ExtendClientAreaChromeHints.NoChrome : Avalonia.Platform.ExtendClientAreaChromeHints.Default;
+    }
+}
